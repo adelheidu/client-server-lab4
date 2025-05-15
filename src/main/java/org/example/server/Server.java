2743e97f10a9c56efcd48c1b362b6e9e209803df
Алина Штейнбах <shteinbakh10@mail.ru>
2025-05-15 19:40:28 +0700

    add Server.java and build.gradle

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,10 +1,11 @@
-package org.example;
+package org.example.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Server.class, args);
     }
 }
